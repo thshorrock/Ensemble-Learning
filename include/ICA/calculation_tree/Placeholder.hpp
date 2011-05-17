d84977b44e897ce3b62fcb7e40d0a76c79dbd77b
Tom Shorrock <t.h.shorrock@gmail.com>
2011-05-17 19:55:01 +0100

    Improved interface of Placement and added documentation

--- include/ICA/calculation_tree/Placeholder.hpp
+++ include/ICA/calculation_tree/Placeholder.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-//#include "symbolic/visitor/visitor.hpp"
 
 #include "Functions.hpp"
 #include "FunctionsIterator.hpp"
@@ -12,23 +11,40 @@ namespace ICR{
 
   namespace ICA{
     
-    //forward declaration;
 
-    template<class T>
-    class DetConstant : public Expression<T>
-    {
-    public:
-      T   Evaluate(const SubContext<T>& c) const
-      {
-	return m_value;
-      }
-
-    private:
-      const T m_value;
-    };
-
-
-    /**
+    /**  A placeholder to a VariableNode in a function.
+     *  The purpose of the placeholder is to be able to form an equation that can be used in multiple contexts.
+     *  A placeholder indicates where a particular node will be in the equation.
+     *  It is then linked to the Variable by a Context.
+     *
+     *  @tparam T The type of the stored data.  
+     *   This will be either double or float - with the latter used only for memory contrained models.
+     *
+     *  Example:
+     *  @code 
+     *
+     *  //Create an expression X*Y + Z.
+     *  ExpressionFactory<double> Factory;  // Create a Factory.
+     *  
+     *  Placeholder<double>* X = Factory.placeholder();
+     *  Placeholder<double>* Y = Factory.placeholder();
+     *  Placeholder<double>* Z = Factory.placeholder();
+     *  Expression<double>* XY = Factory.Multiply(X,Y);
+     *  Expression<double>* Expr = Factory.Add(XY,Z);
+     *  
+     *  //Create some variables
+     *  Builder builder;
+     *  Builder::GaussianNode x = Builder.Gaussian(0.01,0.01);
+     *  Builder::GaussianNode y = Builder.Gaussian(0.01,0.01);
+     *  Builder::GaussianNode z = Builder.Gaussian(0.01,0.01);
+     *  
+     *  //Assign this context to the expression.
+     *  Context context;
+     *  context.Assign(X,x);
+     *  context.Assign(Y,y);
+     *  context.Assign(Z,z);
+     *  
+     *  @endcode
      *
      */
     template<class T>
@@ -37,6 +53,15 @@ namespace ICR{
       
       
     public:
+      
+      /** Constructor */
+      Placeholder()
+	: Expression<T>(),
+	  m_parent(0)
+      {}
+      
+    private:
+      
       typedef  FunctionIterator<const Function<T> > const_iterator;
       typedef  FunctionIterator<Function<T> > iterator;
       
@@ -52,97 +77,95 @@ namespace ICR{
 
       typedef typename boost::call_traits<T>::value_type
       data_t;
-      
-      Placeholder()
-	: Expression<T>(),
-	  m_parent(0)
-      {}
-      
+
+      template<class> friend class GaussianModel;
+      template<class> friend class RectifiedGaussianModel;
+
+      /** A forward iterator pointing to the first operator (one below this placeholder).
+       */
       const_iterator
       begin() const
       {
 	return const_iterator(m_parent);
       }
-      
+      /** The iterator just beyond the root operator
+       */
       const_iterator
       end() const
       {
 	return const_iterator();
       }
-
+      
+      /** A forward iterator pointing to the first operator (one below this placeholder).
+       */
       iterator
       begin() 
       {
 	return const_iterator(m_parent);
       }
       
+      /** The iterator just beyond the root operator
+       */
       iterator
       end() 
       {
 	return const_iterator();
       }
       
-      data_t   Evaluate(subcontext_parameter c) const
+      /** Evaluate the expression.
+       *  @param c The subcontext (i.e <x> or <x^2> of the variables that you wish to build)
+       *  @see SubContext
+       */
+      data_t Evaluate(subcontext_parameter c) const
       {
 	return c.Lookup(this);
       }
       
+      /** Set the parent function.  This is needed for inversion, so that we can iterate through tree.*/
       void
       SetParent(function_parameter p)
       {m_parent = p;}
       
+      /** Get the parent function.  This is needed for inversion, so that we can iterate through tree.*/
       function_t
       GetParent() const
       {return m_parent;}
       
+      /** Invert the expression.
+       *  The expression provided by the user is sufficient only to calclulate the messages from ParentNodes to ChildNodes.
+       *  To evaluate the Message from the Child to the Parent, the expression needs to be inverted.
+       *  This function inverts the expression.
+       *  @param rhs The right-hand-side of the expression to be inverted.
+       *  @param c The Subcontext of the expression to be inverted.
+       *  @return A pair of values.
+       *   The first is the rhs minus the summed terms.
+       *   The second is the multiplicative factor on the rest.
+       *   Both these numbers are required by the Models to invert the expression.
+       */
       std::pair<T,T>
       Invert(const T rhs, const SubContext<T> c) const
       {
-	//returns:
-	//The rhs - the summed terms
-	//The multiplicative factor on the rest.
-	//std::cout<<"SubContext= "<<c<<std::endl;
+	/** returns:
+	 *  The rhs - the summed terms
+	 *  The multiplicative factor on the rest.
+	 */
 
-	// T RHSminusSumed;
-	T Factor;
-
-	//Climb down to the root
-	Function<T> const* root;
-	// if  (m_parent==0){
-	//   std::cout<<"SOMETHING WRONG IN PLACEHOLDER.HPP"<<std::endl;
-
-	//   return std::pair<T,T>(rhs, 1);//This shouldn't happen 
-	// }
 	if  (begin()==end()){
 	  std::cout<<"SOMETHING WRONG IN PLACEHOLDER.HPP"<<std::endl;
-	  BOOST_ASSERT(1==2);
-	  return std::pair<T,T>(rhs, 1);//This shouldn't happen 
+	  BOOST_ASSERT(1==2);//This shouldn't happen 
 	}
 	
-
-	// root = m_parent;
-	// while(root->GetParent()!=0)
-	//   { 
-	//     root = root->GetParent();
-	//   }
 	const_iterator itr = begin(); // the first function.
-	// const_iterator peek = begin();
-	// ++peek;
+	//Climb down to the root
 	while(itr!=end())
 	  ++itr;
-	  //	  ++itr;
 	
 	//evaluate the whole tree
-	//T lhs = root->Evaluate(c);
+	//itr.Previous because itr is now at end().
 	T lhs = itr.Previous()->Evaluate(c);
-	//std::cout<<"lhs = "<<lhs<<std::endl;
 
 
 	//if this placeholder is summed, then undo the summation and return
-	// if (m_parent->GetFunctionType()==FunctionName::PLUS)
-	//   {
-	//     return std::pair<T,T>(rhs - (lhs - c.Lookup(this)), 1);
-	//   }
 	if (begin()->GetFunctionType()==FunctionName::PLUS)
 	  {
 	    return std::pair<T,T>(rhs - (lhs - c.Lookup(this)), 1);
@@ -150,63 +173,22 @@ namespace ICR{
 	//Okay so we have some mulitplication.
 	//Need to evaluate the product
 
-	// Function<T> const* p= m_parent;
-	// bool descend = true;
-	// while(descend)
-	//   { 
-	//     std::cout<<"Descend"<<std::endl;
-	    
-	//     Function<T> const* peek_root = p->GetParent(); //gradparent
-	//     if (peek_root==0)
-	//       descend = false;
-	//     else if (peek_root->GetFunctionType()!=FunctionName::TIMES) 
-	//       descend = false;
-	//     else {
-	//       p = peek_root;
-	//     }
-	    
-	//   }
-
-	// T prod = p->Evaluate(c); //evaluate the subexpression. (the product)
-	// std::cout<<"prod = "<<prod<<std::endl;
 
 	itr = begin();
-	// peek = begin();
-	// peek++; //look at next
+	//Decend until we are no long multiplying
 	while(itr!=end() && itr->GetFunctionType()!=FunctionName::TIMES )
 	  { 
 	    ++itr;
 	  }
-	T prod = itr.Previous()->Evaluate(c); //
+	T prod = itr.Previous()->Evaluate(c);  //The product
 	//subtract from total
 	T other = lhs-prod;
 	//Evaluate the divisor 
-	Factor  = prod / c.Lookup(this);
+	T Factor  = prod / c.Lookup(this);
 	//Return
-	//return std::pair<T,T>((rhs-other)/Factor, 1);
 	return std::pair<T,T>((rhs-other), Factor);
-	  //   //std::cout<<"Descend"<<std::endl;
-	  //   const_iterator peek = itr+1;
-	  //   if (peek == end())
-	  //     descend = false;
-	  //   else if (peek->GetFunctionType()!=FunctionName::TIMES) 
-	  //     descend = false;
-	  //   else {
-	  //     itr = peek;
-	  //   }
-	    
-	  // }
-	// while(itr->GetFunctionType()!=FunctionName::TIMES && (itr++)!=end() )
-	//   {}
-
-
-
-	//std::cout<<"other = "<<other<<std::endl;
-	
-	//std::cout<<"Factor = "<<Factor<<std::endl;
       }
 
-    private:
       function_t m_parent;
       friend class SubContext<T>;
     };
