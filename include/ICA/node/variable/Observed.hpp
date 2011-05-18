8cc32e13de91b8d6ce75d53d847c9f64b1c7bf87
Tom Shorrock <t.h.shorrock@gmail.com>
2011-05-18 17:13:43 +0100

    Fixed mistakes in Observed.hpp so that it compiles

--- include/ICA/node/variable/Observed.hpp
+++ include/ICA/node/variable/Observed.hpp
@@ -1,7 +1,11 @@
 #pragma once
 #include "ICA/message/Moments.hpp"
 #include "ICA/node/Node.hpp"
-#include "ICA/node/variable/ConstantModels.hpp"
+#include "ICA/exponential_model/Gaussian.hpp"
+#include "ICA/exponential_model/RectifiedGaussian.hpp"
+#include "ICA/exponential_model/Gamma.hpp"
+#include "ICA/exponential_model/Discrete.hpp"
+#include "ICA/exponential_model/Dirichlet.hpp"
 
 namespace ICR{
   namespace ICA{
@@ -12,13 +16,13 @@ namespace ICR{
     public:
       
       ObservedNode( const T& value)
-	: m_Moments(2, make_Moments(value), Model() ), 
+	: m_Moments(make_Moments(2, value, Model() ) ), 
 	  m_parent(0),
 	  m_children()
       {}
 
       ObservedNode(const size_t& elements, const T& value)
-	: m_Model(make_Moments(elements,value)), 
+	: m_Moments(make_Moments(elements,value, Model() ) ), 
 	  m_parent(0), 
 	  m_children()
       {}
@@ -32,7 +36,7 @@ namespace ICR{
       AddChildFactor(FactorNode<T>* f);
       
       void
-      InitialiseMoments(){}
+      InitialiseMoments(){};
 
       const Moments<T>&
       GetMoments() const;
@@ -40,27 +44,27 @@ namespace ICR{
       
       //No contribution to cost, nothing to update
       void 
-      Iterate(Coster& C){};
+      Iterate(Coster& C);
       
     private:
       //default
       Moments<T>
-      make_Moments(const size_t s,const T& d, const Gaussian)
+      make_Moments(const size_t s,const T& d, const Gaussian<T> )
       {
 	return Moments<T>(d, d*d);
       }
       Moments<T>
-      make_Moments(const size_t s,const T& d, const RectifiedGaussian)
+      make_Moments(const size_t s,const T& d, const RectifiedGaussian<T>)
       {
 	return Moments<T>(d, d*d);
       }
       Moments<T>
-      make_Moments(const size_t s,const T& d, const Gamma)
+      make_Moments(const size_t s,const T& d, const Gamma<T>)
       {
 	return Moments<T>(d, std::log(d));
       }
       Moments<T>
-      make_Moments(const size_t s,const T& d, const Dirichlet)
+      make_Moments(const size_t s,const T& d, const Dirichlet<T>)
       {
 	return Moments<T>(std::vector<T>(s,d));
       }
@@ -78,7 +82,7 @@ const ICR::ICA::Moments<T>&
 ICR::ICA::ObservedNode<model,T>::GetMoments() const
 {
   //Model is thread safe
-  return m_Model.GetMoments();
+  return m_Moments;
 }
 
 template<class model,class T>
@@ -87,7 +91,7 @@ void
 ICR::ICA::ObservedNode<model,T>::AddChildFactor(FactorNode<T>* f)
 {
   //Could be many factors, potentially added with many threads
-  boost::lock_guard<boost::mutex> lock(m_mutex);
+  // boost::lock_guard<boost::mutex> lock(m_mutex);
   m_children.push_back(f);
 }
     
@@ -95,7 +99,7 @@ ICR::ICA::ObservedNode<model,T>::AddChildFactor(FactorNode<T>* f)
 template<class Model, class T>
 inline
 void
-ICR::ICA::ObvservedNode<Model,T>::SetParentFactor(FactorNode<T>* f)
+ICR::ICA::ObservedNode<Model,T>::SetParentFactor(FactorNode<T>* f)
 {
   //Assuming only one factor: this should be called once per iteration 
   //  and therefore be thread safe.  
