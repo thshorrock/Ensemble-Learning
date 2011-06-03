de592a89ccbb16816068dc8ad8ea373fa2da2bfd
Tom Shorrock <t.h.shorrock@gmail.com>
2011-06-03 14:41:24 +0100

    added dependancies and license

--- EnsembleLearning-src/include/EnsembleLearning/message/MomentsIterator.hpp
+++ EnsembleLearning-src/include/EnsembleLearning/message/MomentsIterator.hpp
@@ -1,10 +1,42 @@
 #pragma once
+#ifndef MOMENTSITERATOR_HPP
+#define MOMENTSITERATOR_HPP
+
+
+
+/***********************************************************************************
+ ***********************************************************************************
+ **                                                                               **
+ **  Copyright (C) 2011 Tom Shorrock <t.h.shorrock@gmail.com> 
+ **                                                                               **
+ **                                                                               **
+ **  This program is free software; you can redistribute it and/or                **
+ **  modify it under the terms of the GNU General Public License                  **
+ **  as published by the Free Software Foundation; either version 2               **
+ **  of the License, or (at your option) any later version.                       **
+ **                                                                               **
+ **  This program is distributed in the hope that it will be useful,              **
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of               **
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                **
+ **  GNU General Public License for more details.                                 **
+ **                                                                               **
+ **  You should have received a copy of the GNU General Public License            **
+ **  along with this program; if not, write to the Free Software                  **
+ **  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.  **
+ **                                                                               **
+ ***********************************************************************************
+ ***********************************************************************************/
+
+
+
 #include "EnsembleLearning/detail/Mutex.hpp"
+
 #include <boost/iterator/iterator_facade.hpp>
 #include <boost/type_traits/is_convertible.hpp>
 #include <boost/type_traits/is_same.hpp>
 #include <boost/utility/enable_if.hpp>
 #include <boost/call_traits.hpp>
+
 namespace ICR{
   namespace EnsembleLearning{
     
@@ -193,3 +225,4 @@ namespace ICR{
     
   }
 }
+#endif  // guard for MOMENTSITERATOR_HPP
