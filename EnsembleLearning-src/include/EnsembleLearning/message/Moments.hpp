57639f690e7fcce9f78f0300f3c33c793e7416a7
Tom Shorrock <t.h.shorrock@gmail.com>
2011-06-03 14:33:30 +0100

    added dependancies and license

--- EnsembleLearning-src/include/EnsembleLearning/message/Moments.hpp
+++ EnsembleLearning-src/include/EnsembleLearning/message/Moments.hpp
@@ -1,9 +1,38 @@
 #pragma once
-//#include "NaturalParameters.hpp"
+#ifndef MOMENTS_HPP
+#define MOMENTS_HPP
+
+
+/***********************************************************************************
+ ***********************************************************************************
+ **                                                                               **
+ **  Copyright (C) 2011 Tom Shorrock <t.h.shorrock@gmail.com> 
+ **                                                                               **
+ **                                                                               **
+ **  This program is free software; you can redistribute it and/or                **
+ **  modify it under the terms of the GNU General Public License                  **
+ **  as published by the Free Software Foundation; either version 2               **
+ **  of the License, or (at your option) any later version.                       **
+ **                                                                               **
+ **  This program is distributed in the hope that it will be useful,              **
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of               **
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                **
+ **  GNU General Public License for more details.                                 **
+ **                                                                               **
+ **  You should have received a copy of the GNU General Public License            **
+ **  along with this program; if not, write to the Free Software                  **
+ **  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.  **
+ **                                                                               **
+ ***********************************************************************************
+ ***********************************************************************************/
+
+
+
 #include "MomentsIterator.hpp"
 #include "EnsembleLearning/node/Node.hpp"
 #include "EnsembleLearning/detail/Mutex.hpp"
 #include "EnsembleLearning/detail/parallel_algorithms.hpp"
+
 #include <boost/assign/list_of.hpp>
 #include <boost/assign/std/vector.hpp>
 #include <iostream>
@@ -417,5 +446,5 @@ ICR::EnsembleLearning::Moments<T>::operator*=(data_parameter d)
   return *this;
 }
       
-   
 
+#endif  // guard for MOMENTS_HPP
