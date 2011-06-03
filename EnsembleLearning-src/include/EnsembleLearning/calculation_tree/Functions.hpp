6b34971112c86fe6e2dbabc108c83bc055638137
Tom Shorrock <t.h.shorrock@gmail.com>
2011-06-03 13:44:29 +0100

    added license to Functions (and modified dependancies)

--- EnsembleLearning-src/include/EnsembleLearning/calculation_tree/Functions.hpp
+++ EnsembleLearning-src/include/EnsembleLearning/calculation_tree/Functions.hpp
@@ -1,11 +1,46 @@
+
+/***********************************************************************************
+ ***********************************************************************************
+ **                                                                               **
+ **  Copyright (C) 2011 Tom Shorrock <t.h.shorrock@gmail.com> 
+ **                                                                               **
+ **                                                                               **
+ **  This program is free software; you can redistribute it and/or                **
+ **  modify it under the terms of the GNU General Public License                  **
+ **  as published by the Free Software Foundation; either version 2               **
+ **  of the License, or (at your option) any later version.                       **
+ **                                                                               **
+ **  This program is distributed in the hope that it will be useful,              **
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of               **
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                **
+ **  GNU General Public License for more details.                                 **
+ **                                                                               **
+ **  You should have received a copy of the GNU General Public License            **
+ **  along with this program; if not, write to the Free Software                  **
+ **  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.  **
+ **                                                                               **
+ ***********************************************************************************
+ ***********************************************************************************/
+
+
+
 #pragma once
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+
+
 #include "Expression.hpp"
-#include "Context.hpp"
+#include <boost/call_traits.hpp>
 
 
 namespace ICR{
 
   namespace EnsembleLearning{
+
+    //forward declare
+    
+    template<class T>
+    class SubContext;
 
     /** Add two functions together.
      *  @tparam T The data type, either float or double.
@@ -218,3 +253,5 @@ ICR::EnsembleLearning::Times<T>::Evaluate(subcontext_parameter c) const
     m_a -> Evaluate(c) *
     m_b -> Evaluate(c);
 }
+
+#endif  // guard for FUNCTIONS_HPP
