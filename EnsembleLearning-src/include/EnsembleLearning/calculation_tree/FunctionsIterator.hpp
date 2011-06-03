334acad5eb2b88797f1f324e2f7a047313ac50b0
Tom Shorrock <t.h.shorrock@gmail.com>
2011-06-03 13:48:48 +0100

    added license and simplified dependancy

--- EnsembleLearning-src/include/EnsembleLearning/calculation_tree/FunctionsIterator.hpp
+++ EnsembleLearning-src/include/EnsembleLearning/calculation_tree/FunctionsIterator.hpp
@@ -1,6 +1,34 @@
-#pragma once
 
-#include "Functions.hpp"
+/***********************************************************************************
+ ***********************************************************************************
+ **                                                                               **
+ **  Copyright (C) 2011 Tom Shorrock <t.h.shorrock@gmail.com> 
+ **                                                                               **
+ **                                                                               **
+ **  This program is free software; you can redistribute it and/or                **
+ **  modify it under the terms of the GNU General Public License                  **
+ **  as published by the Free Software Foundation; either version 2               **
+ **  of the License, or (at your option) any later version.                       **
+ **                                                                               **
+ **  This program is distributed in the hope that it will be useful,              **
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of               **
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                **
+ **  GNU General Public License for more details.                                 **
+ **                                                                               **
+ **  You should have received a copy of the GNU General Public License            **
+ **  along with this program; if not, write to the Free Software                  **
+ **  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.  **
+ **                                                                               **
+ ***********************************************************************************
+ ***********************************************************************************/
+
+
+
+#pragma once
+#ifndef FUNCTIONSITERATOR_HPP
+#define FUNCTIONSITERATOR_HPP
+
+#include "Expression.hpp"
 
 #include <boost/iterator/iterator_facade.hpp>
 #include <boost/type_traits/is_convertible.hpp>
@@ -86,9 +114,8 @@ namespace ICR{
       function* m_function;
       function* m_previous;
     };
-    // typedef FunctionIterator<Function<T> > function_itereator;
-    // typedef FunctionIterator<const Function<T> > const_function_iterator;
-
     
   }
 }
+
+#endif  // guard for FUNCTIONSITERATOR_HPP
