fe2d2fbd0bf4f84301dff6dff3cfa4cacef581ed
Tom Shorrock <t.h.shorrock@gmail.com>
2011-06-03 14:32:08 +0100

    added dependancies and license

--- EnsembleLearning-src/include/EnsembleLearning/message/Coster.hpp
+++ EnsembleLearning-src/include/EnsembleLearning/message/Coster.hpp
@@ -1,4 +1,34 @@
 #pragma once
+#ifndef COSTER_HPP
+#define COSTER_HPP
+
+
+/***********************************************************************************
+ ***********************************************************************************
+ **                                                                               **
+ **  Copyright (C) 2011 Tom Shorrock <t.h.shorrock@gmail.com> 
+ **                                                                               **
+ **                                                                               **
+ **  This program is free software; you can redistribute it and/or                **
+ **  modify it under the terms of the GNU General Public License                  **
+ **  as published by the Free Software Foundation; either version 2               **
+ **  of the License, or (at your option) any later version.                       **
+ **                                                                               **
+ **  This program is distributed in the hope that it will be useful,              **
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of               **
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                **
+ **  GNU General Public License for more details.                                 **
+ **                                                                               **
+ **  You should have received a copy of the GNU General Public License            **
+ **  along with this program; if not, write to the Free Software                  **
+ **  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.  **
+ **                                                                               **
+ ***********************************************************************************
+ ***********************************************************************************/
+
+
+
+
 #include "EnsembleLearning/detail/Mutex.hpp"
 #include <boost/call_traits.hpp>
 
@@ -61,3 +91,4 @@ namespace ICR {
     
   }
 }
+#endif  // guard for COSTER_HPP
