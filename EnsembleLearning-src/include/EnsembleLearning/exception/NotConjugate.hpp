193ee1cc46abb7a857b3da34f44405ec4c4e5b0c
Tom Shorrock <t.h.shorrock@gmail.com>
2011-06-03 14:12:24 +0100

    added license

--- EnsembleLearning-src/include/EnsembleLearning/exception/NotConjugate.hpp
+++ EnsembleLearning-src/include/EnsembleLearning/exception/NotConjugate.hpp
@@ -1,6 +1,35 @@
 #pragma once
+#ifndef NOTCONJUGATE_HPP
+#define NOTCONJUGATE_HPP
 
-namespace ICR{
+
+
+/***********************************************************************************
+ ***********************************************************************************
+ **                                                                               **
+ **  Copyright (C) 2011 Tom Shorrock <t.h.shorrock@gmail.com> 
+ **                                                                               **
+ **                                                                               **
+ **  This program is free software; you can redistribute it and/or                **
+ **  modify it under the terms of the GNU General Public License                  **
+ **  as published by the Free Software Foundation; either version 2               **
+ **  of the License, or (at your option) any later version.                       **
+ **                                                                               **
+ **  This program is distributed in the hope that it will be useful,              **
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of               **
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                **
+ **  GNU General Public License for more details.                                 **
+ **                                                                               **
+ **  You should have received a copy of the GNU General Public License            **
+ **  along with this program; if not, write to the Free Software                  **
+ **  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.  **
+ **                                                                               **
+ ***********************************************************************************
+ ***********************************************************************************/
+
+
+
+Namespace ICR{
   namespace EnsembleLearning{
     namespace Exception{
       /** An exception thrown when a message is requested from a node that does not have a conjugate variable.
@@ -10,3 +39,4 @@ namespace ICR{
     }
   }
 }
+#endif  // guard for NOTCONJUGATE_HPP
